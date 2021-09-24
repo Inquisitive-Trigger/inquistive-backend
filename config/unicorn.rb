5f78f953e331384086a7b9c891178262f6e7daee
shinter61 <shinter61@gmail.com>
2021-09-24 18:07:47 +0900

    Fix app path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ $worker  = 2
 #何秒経過すればワーカーを削除するのかを決める
 $timeout = 30
 #自分のアプリケーション名
-$app_dir = "/home/ec2-user/projects/inquisitive-backend"
+$app_dir = "/home/ec2-user/projects/inquistive-backend"
 #リクエストを受け取るポート番号を指定
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 #PIDの管理ファイルディレクトリ
