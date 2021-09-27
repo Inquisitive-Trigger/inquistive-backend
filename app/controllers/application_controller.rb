4435c3d0c9c9e2cae5bbad652509bee6e5d9bcf2
shinter61 <shinter61@gmail.com>
2021-09-27 09:34:52 +0900

    Authenticate request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  
+  before_action :authenticate
+
   def index
     render json: { status: 200, message: "Hello, we are inquisitive!!" }
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      auth_user = User.find_by(token: token)
+      auth_user.present?
+    end
   end
 end
