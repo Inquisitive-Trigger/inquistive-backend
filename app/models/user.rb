e60292066cf2dc15584b60d8ca138dfa77836f1b
shinter61 <shinter61@gmail.com>
2021-09-28 10:38:27 +0900

    Add unique constraint on user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :application_companies, dependent: :destroy
 
+  validates :email, uniqueness: true
+
   def searcher?
     purpose
   end
